80ce3c0b09a4c0b847e5ad5ae68b9f072634c2fb
carolinewilson <carolinecwilson3+github@gmail.com>
2017-01-03 17:25:17 +0000

    Adding final projects and new screen shots

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# portfolio
+# Caroline Wilson: Portfolio 
+
+## About
+I'm a junior full stack web developer living in London.
+
+## Tech
+
+* javascript
+* jQuery
+* HTML5
+* SCSS / CSS3
+* AngularJS (v1)
+* Node.js
+* Express.js
+* MongoDB
+* Ruby on Rails
+
+## Projects
+Whilst studying web development at General Assembly I've completed a number of projects in addition to personal projects.
+
+* [Gem Match Puzzle] ()
+* [[ Meet ] in the Middle] ()
+* [Danielle Hammersley - Interior Desinger] ()
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-const gulp     	 = require("gulp");
+const gulp     	 = require('gulp');
 const sass 		 	 = require('gulp-sass');
-const babel      = require("gulp-babel");
-const cleanCSS 	 = require("gulp-clean-css");
+const babel      = require('gulp-babel');
+const cleanCSS 	 = require('gulp-clean-css');
 const flatten		 = require('gulp-flatten');
 const plumber    = require('gulp-plumber');
-const nodemon    = require("gulp-nodemon");
+const nodemon    = require('gulp-nodemon');
 const livereload = require('gulp-livereload');
 
-const src  = "src";
-const dist = "public";
+const src  = 'src';
+const dist = 'public';
 
-gulp.task("es6", () => {
+gulp.task('es6', () => {
 	return gulp.src(`${src}/**/*.js`)
 		.pipe(plumber())
 		.pipe(babel({
-			presets: ["es2015"]
+			presets: ['es2015']
 		}))
 		.pipe(gulp.dest('public'));
 });
@@ -31,20 +31,20 @@ gulp.task('nodemon', () => {
 gulp.task('sass', () => {
 	return gulp.src(`${src}/**/*.scss`)
     .pipe(sass().on('error', sass.logError))
-		.pipe(cleanCSS({ compatibility: "ie8"}))
+		.pipe(cleanCSS({ compatibility: 'ie8'}))
     .pipe(plumber())
     .pipe(flatten())
     .pipe(gulp.dest(`${dist}/css/`))
     .pipe(livereload());
 });
 
-gulp.task("watch", () => {
+gulp.task('watch', () => {
 	livereload.listen();
 	gulp.watch(`${src}/**/*.scss`, ['sass']);
   gulp.watch(`${src}/**/*.js`, ['es6']);
 });
 
-gulp.task("default", [
+gulp.task('default', [
   'es6',
   'sass',
   'watch',
